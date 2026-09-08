3bd9b6d48c4e997e212c31fc1208430449c8d326
Sahil Gandhi <sahilmgandhi@gmail.com>
2026-09-08 09:09:01 -0700

    Fill light: raise hemi/rim so backlit faces keep detail

--- age-of-war-3d/src/lighting/lighting.js
+++ age-of-war-3d/src/lighting/lighting.js
@@ -7,11 +7,11 @@
 import * as THREE from 'three';
 
 const AGE_MOOD = [
-  { sun: 0xffdfb2, sunI: 2.6, pos: [4, 15, 13], sky: 0x9a86b8, gnd: 0x5a4a3a, hemiI: 0.55, rim: 0xb0c4de, rimI: 0.45 },
+  { sun: 0xffdfb2, sunI: 2.6, pos: [4, 15, 13], sky: 0x9a86b8, gnd: 0x5a4a3a, hemiI: 0.7, rim: 0xb0c4de, rimI: 0.85 },
   { sun: 0x8fa8ff, sunI: 1.7, pos: [-7, 12, 9], sky: 0x2a3a6e, gnd: 0x141a28, hemiI: 0.62, rim: 0x5a76c8, rimI: 0.65 },
-  { sun: 0xffe8c0, sunI: 2.8, pos: [8, 16, 10], sky: 0xa898c0, gnd: 0x6a5a44, hemiI: 0.55, rim: 0xc8b49a, rimI: 0.45 },
-  { sun: 0xd8e0ea, sunI: 2.0, pos: [0, 18, 6], sky: 0x8a94a8, gnd: 0x4a4a52, hemiI: 0.6, rim: 0x9aa4b8, rimI: 0.5 },
-  { sun: 0xbfa8ff, sunI: 2.2, pos: [-8, 14, 12], sky: 0x6a5ac8, gnd: 0x2a2a44, hemiI: 0.5, rim: 0x8a7ae8, rimI: 0.55 },
+  { sun: 0xffe8c0, sunI: 2.8, pos: [8, 16, 10], sky: 0xa898c0, gnd: 0x6a5a44, hemiI: 0.65, rim: 0xc8b49a, rimI: 0.7 },
+  { sun: 0xd8e0ea, sunI: 2.0, pos: [0, 18, 6], sky: 0x8a94a8, gnd: 0x4a4a52, hemiI: 0.7, rim: 0x9aa4b8, rimI: 0.7 },
+  { sun: 0xbfa8ff, sunI: 2.2, pos: [-8, 14, 12], sky: 0x6a5ac8, gnd: 0x2a2a44, hemiI: 0.6, rim: 0x8a7ae8, rimI: 0.7 },
 ];
 
 export function createLighting(scene) {
